f13c01bf11ffb6cc2d248f53e35df8156147a273
GizemBiliktue <Gizembiliktu@gmail.com>
2024-07-03 17:34:46 +0200

    LED through touch

--- Plantoji.ino
+++ Plantoji.ino
@@ -1,50 +1,56 @@
-/*
- * Bibliotheken 
- */
-//#include <LiquidCrystal.h> //Funktioniert nicht ... BZW schon aber nicht wie wir wollen vielleicht austauschen ??
 #include <Adafruit_CircuitPlayground.h>
 
-//Soil Moisture
+// Globale Variablen für LED
+int threshold = 5;
+int current = 0;
+
+// Pins für Bodenfeuchtigkeitssensor
 const int moisturePin = 10;
 const int moisturePowerPin = 0;
-const int soilMoistLevelLow = 200;  
-const int soilMoistLevelHigh = 300; 
+const int soilMoistLevelLow = 200;
+const int soilMoistLevelHigh = 300;
 
-/** --------------------------------------------------
- *  ------------  Set UP Method       ----------------
- *  --------------------------------------------------
- */
 void setup() {
-   CircuitPlayground.begin();
-  // Serialization with Baudrate 9600
   Serial.begin(9600);
-
-  /*
-      initialise : Bodenfeuchtigkeit
-  */
-  pinMode(moisturePin,INPUT);
-  pinMode(moisturePowerPin,OUTPUT);
-
+  CircuitPlayground.begin();
+  CircuitPlayground.setBrightness(40);
+  pinMode(moisturePin, INPUT);
+  pinMode(moisturePowerPin, OUTPUT);
 }
 
-
-
-/** --------------------------------------------------
- *  ------------  Ausführungmethode   ----------------
- *  --------------------------------------------------
- */
 void loop() {
+  controlLED();          // Steuerung der LEDs
+  measureMoisture();     // Messung der Bodenfeuchtigkeit
+  measureLight();        // Messung des Lichtwerts
+}
 
-  /*
-     Bodenfeuchtigkeit
-  */
-  digitalWrite(moisturePowerPin,HIGH);
+/*
+LED
+*/
+void controlLED() {
+  current = CircuitPlayground.readCap(1);
+  Serial.print("Touch-sensitivity: ");
+  Serial.println(current);
+  
+  for (int i = 0; i < 10; i++) {
+    if (current > threshold * (i + 1)) {
+      CircuitPlayground.setPixelColor(i, 255, 0, 0);  // Rot, wenn Schwellenwert überschritten
+    } else {
+      CircuitPlayground.setPixelColor(i, 0, 255, 0);  // Grün, wenn Schwellenwert nicht überschritten
+    }
+  }
+}
+
+/*
+  Bodenfeuchtigkeit
+*/
+void measureMoisture() {
+  digitalWrite(moisturePowerPin, HIGH);
   delay(100);
   int soilMoist = analogRead(moisturePin);
   Serial.print("Analog Value: ");
   Serial.print(soilMoist);
   
-  // Auswertung der Bodenfeuchtigkeit : 200-300 Perfekt für Pflanze 
   if (soilMoist > soilMoistLevelHigh) {
     Serial.println(" Status: Der Boden ist zu feucht");
   } else if (soilMoist >= soilMoistLevelLow && soilMoist < soilMoistLevelHigh) {
@@ -52,18 +58,15 @@ void loop() {
   } else {
     Serial.println(" Status: Der Boden ist zu trocken");
   }
-  digitalWrite(moisturePowerPin, LOW);  
-  delay(2000); 
+  
+  digitalWrite(moisturePowerPin, LOW);
+}
 
-
-  /*
-     Lichtwerte
-  */
+/*
+  Licht
+*/
+void measureLight() {
   float lux = CircuitPlayground.lightSensor();
   Serial.print("Lichtwert: ");
   Serial.println(lux);
-  
- delay(1000);
-
-
 }
