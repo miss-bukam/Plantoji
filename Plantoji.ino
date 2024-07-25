5bf20542422f5cddffa8ab4597cae3c87c8a0f8c
miss-bukam <115629298+miss-bukam@users.noreply.github.com>
2024-07-25 11:04:58 +0200

    Update Plantoji.ino
    last Changes

--- Plantoji.ino
+++ Plantoji.ino
@@ -3,15 +3,13 @@
 // Globale Variablen für LED
 int threshold = 15;
 int current = 0;
-
-int topf = 0;
+int topf = 12;
 
 // Pins für Bodenfeuchtigkeitssensor
-const int moisturePin = 10;
-const int moisturePowerPin = 0;
+const int moisturePin = A0; 
+const int moisturePowerPin = 7; 
 const int soilMoistLevelLow = 200;
 const int soilMoistLevelHigh = 300;
-
 
 void setup() {
   Serial.begin(9600);
@@ -26,8 +24,7 @@ void loop() {
   measureLight();        // Messung des Lichtwerts
   controlLED();          // Steuerung der LEDs
   checkTouch();          // Überprüfung, ob der Topf angefasst wird
-  delay(100);  // Kurze Pause, um die Serielle Kommunikation nicht zu überlasten
-
+  delay(1000);  // 1 Sekunde Pause
 }
 
 /*
@@ -57,31 +54,30 @@ void checkTouch() {
   
   if (topf > 5) {
     Serial.println("Topf angefasst");
-    CircuitPlayground.setPixelColor(0, 0, 0, 255);  // Zeige Rot auf der ersten LED an
+    CircuitPlayground.setPixelColor(0, 0, 0, 255);  // Zeige Blau auf der ersten LED an
   } else {
     Serial.println("Topf nicht angefasst");
-    CircuitPlayground.setPixelColor(0, 0, 0, 0);  // Zeige Grün auf der ersten LED an, wenn nicht angefasst
+    CircuitPlayground.setPixelColor(0, 0, 0, 0);  // Schalte LED aus, wenn nicht angefasst
   }
 }
-
-
 
 /*
   Bodenfeuchtigkeit
 */
 void measureMoisture() {
   digitalWrite(moisturePowerPin, HIGH);
-  delay(100);
+  delay(10); // Warte kurz, um sicherzustellen, dass der Sensor Strom bekommt
+  
   int soilMoist = analogRead(moisturePin);
   Serial.print("Analog Value: ");
-  Serial.print(soilMoist);
+  Serial.println(soilMoist);
   
   if (soilMoist > soilMoistLevelHigh) {
-    Serial.println(" Status: Der Boden ist zu feucht");
+    Serial.println("Status: Der Boden ist zu feucht");
   } else if (soilMoist >= soilMoistLevelLow && soilMoist < soilMoistLevelHigh) {
-    Serial.println(" Status: Die Bodenfeuchtigkeit ist perfekt");
+    Serial.println("Status: Die Bodenfeuchtigkeit ist perfekt");
   } else {
-    Serial.println(" Status: Der Boden ist zu trocken");
+    Serial.println("Status: Der Boden ist zu trocken");
   }
   
   digitalWrite(moisturePowerPin, LOW);
